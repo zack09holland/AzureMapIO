bc1e202dbdd7bfb55c0e23fd8715eecf69899b1a
Zach Holland <zack09holland@gmail.com>
2020-03-19 21:25:20 -0700

    Had to recreate the web app with node.js due to limitations

---
+++ nodeAzureApp/app/public/js/MapLayers/addChoropleth.js
@@ -0,0 +1,84 @@
+
+
+function addChoropleth() {
+
+    var popup, maxValue = 500;
+
+    var defaultColor = '#FFEDA0';
+    var colorScale = [
+        10, '#FED976',
+        20, '#FEB24C',
+        50, '#FD8D3C',
+        100, '#FC4E2A',
+        200, '#E31A1C',
+        500, '#BD0026',
+        1000, '#800026'
+    ];
+
+    //Create a popup but leave it closed so we can update it and display it later.
+    popup = new atlas.Popup({
+        position: [0, 0]
+    });
+
+    //Create a data source and add it to the map.
+    datasource = new atlas.source.DataSource();
+    map.sources.add(datasource);
+
+    //Create a stepped expression based on the color scale. 
+    var steppedExp = [
+        'step',
+        ['get', 'density'],
+        defaultColor
+    ];
+
+    steppedExp = steppedExp.concat(colorScale);
+
+    //Create a layer to render the polygon data.
+    var polygonLayer = new atlas.layer.PolygonLayer(datasource, null, {
+        fillColor: steppedExp
+    });
+    map.layers.add(polygonLayer, 'labels');
+
+    //Add a mouse move event to the polygon layer to show a popup with information.
+    map.events.add('mousemove', polygonLayer, function (e) {
+        if (e.shapes && e.shapes.length > 0) {
+            var properties = e.shapes[0].getProperties();
+
+            //Update the content of the popup.
+            popup.setOptions({
+                content: '<div style="padding:10px"><b>' + properties.name + '</b><br/>Population Density: ' + properties.density + ' people/mi<sup>2</sup></div>',
+                position: e.position
+            });
+
+            //Open the popup.
+            popup.open(map);
+        }
+    });
+
+    //Add a mouse leave event to the polygon layer to hide the popup.
+    map.events.add('mouseleave', polygonLayer, function (e) {
+        popup.close();
+    });
+
+    //Download a GeoJSON feed and add the data to the data source.
+    datasource.importDataFromUrl('data/geojson/US_States_Population_Density.json');
+    // datasource.importDataFromUrl(usPopDensity);
+
+
+    function createLegend() {
+        var html = [];
+
+        html.push('<i style="background:', defaultColor, '"></i> 0-', colorScale[0], '<br/>');
+        
+        for (var i = 0; i < colorScale.length; i += 2) {
+            html.push(
+                '<i style="background:', (colorScale[i + 1]), '"></i> ',
+                colorScale[i], (colorScale[i + 2] ? '&ndash;' + colorScale[i + 2] + '<br/>' : '+')
+            );
+        }
+
+        document.getElementById('legend').innerHTML += html.join('');
+    }
+        //Create a legend.
+        createLegend();
+}
---
+++ nodeAzureApp/app/public/js/main.js
@@ -0,0 +1,41 @@
+
+var map, drawingManager, controls = [];
+//Note that the typeahead parameter is set to true.
+var geocodeServiceUrlTemplate = 'https://atlas.microsoft.com/search/{searchType}/json?typeahead=true&subscription-key={subscription-key}&api-version=1&query={query}&language={language}&lon={lon}&lat={lat}&countrySet={countrySet}&view=Auto';
+// Use SubscriptionKeyCredential with a subscription key
+var subscriptionKeyCredential = new atlas.service.SubscriptionKeyCredential(atlas.getSubscriptionKey());
+
+// Use subscriptionKeyCredential to create a pipeline
+var pipeline = atlas.service.MapsURL.newPipeline(subscriptionKeyCredential);
+
+// Construct the SearchURL object
+var searchURL = new atlas.service.SearchURL(pipeline);
+
+function GetMap() {
+    console.log("we made it")
+    //Initialize a map instance.
+    map = new atlas.Map('myMap', {
+        // center: [-98.493928, 38.096798],
+        // zoom: 4,
+        // view: "Auto",
+        center: [-122.33, 47.6],
+        zoom: 12,
+        view: 'Auto',
+
+        //Add your Azure Maps subscription key to the map SDK. Get an Azure Maps key at https://azure.com/maps
+        authOptions: {
+            authType: 'subscriptionKey',
+            subscriptionKey: 'TfUWvWqVnTGKGMcIvxr5coNt7eiWrKxh6wJe0keVZSs'
+        }
+    });
+
+    //Wait until the map resources are ready.
+
+    map.events.add('ready', function () {
+        // mapControls()
+        // drawingTools();
+        // mapSearch();
+
+        addChoropleth();
+    });
+}
