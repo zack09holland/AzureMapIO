53fa90d5e21622df2bb2fa8359d5fef21c105601
Zach Holland <zack09holland@gmail.com>
2020-03-21 21:33:18 -0700

    Got the choropleth animated layer working

--- nodeAzureApp/app/public/js/MapLayers/Choropleth/addChoropleths.js
+++ nodeAzureApp/app/public/js/MapLayers/Choropleth/addChoropleths.js
@@ -1,3 +1,4 @@
+//Variables to be used by the create legend function
 var choroplethLayer, extrudedPolygonLayer;
 var defaultColor = '';
 var colorScale = [];
@@ -5,7 +6,6 @@ var colorScale = [];
 
 function addChoropleth() {
 
-    var popup, maxValue = 500;
     choroplethLayer;
     defaultColor = '#FFEDA0';
     colorScale = [
@@ -26,7 +26,7 @@ function addChoropleth() {
     });
 
     //Create a popup but leave it closed so we can update it and display it later.
-    popup = new atlas.Popup({
+    var popup = new atlas.Popup({
         position: [0, 0]
     });
 
@@ -148,6 +148,233 @@ function createLegend(id) {
 }
 
 
+var isPaused = true;
+var timer;
+function addAnimatedChoropleth() {
+
+    var popup, maxScale = 30,
+        colorExpressions = [];
+
+    //Create a popup but leave it closed so we can update it and display it later.
+    popup = new atlas.Popup({
+        position: [0, 0]
+    });
+
+    //Create a data source and add it to the map.
+    datasource = new atlas.source.DataSource();
+    map.sources.add(datasource);
+
+    //Create an array of expressions to define the fill color based on 
+    for (var i = 0; i <= 10; i++) {
+        colorExpressions.push([
+            'interpolate',
+            ['linear'],
+            ['get', 'PopChange' + i],
+            -maxScale, 'rgb(255,0,255)', // Magenta
+            -maxScale / 2, 'rgb(0,0,255)', // Blue
+            0, 'rgb(0,255,0)', // Green
+            maxScale / 2, 'rgb(255,255,0)', // Yellow
+            maxScale, 'rgb(255,0,0)' // Red
+        ]);
+    }
+
+    //Create a layer to render the polygon data.
+    var animatedChoroplethLayer = new atlas.layer.PolygonLayer(datasource, null, {
+        fillColor: colorExpressions[0]
+    });
+    map.layers.add(animatedChoroplethLayer, 'labels');
+
+    //Add a mouse move event to the polygon layer to show a popup with information.
+    map.events.add('mousemove', animatedChoroplethLayer, function (e) {
+        if (e.shapes && e.shapes.length > 0) {
+            var properties = e.shapes[0].getProperties();
+
+            var html = ['<div style="padding:10px"><b>', properties.CountyName, '</b><br />', properties.StateName, '<br />% Pop Change:<br/><table><tr><td>Year</td><td>% Change</td></tr>'];
+
+            for (var i = 0; i <= 10; i++) {
+                var year = 2000 + i;
+                html.push('<tr><td>', year, '</td><td>', properties['PopChange' + i], '%</td></tr>');
+            }
+
+            html.push('</table></div>');
+
+            //Update the content of the popup.
+            popup.setOptions({
+                content: html.join(''),
+                position: e.position
+            });
+
+            //Open the popup.
+            popup.open(map);
+        }
+    });
+
+    //Add a mouse leave event to the polygon layer to hide the popup.
+    map.events.add('mouseleave', animatedChoroplethLayer, function (e) {
+        popup.close();
+    });
+
+    //TODO: Update to use spatial io module.
+
+    //Download population estimates for US counties.
+    fetch('data/US_County_2010_Population.csv')
+        .then(response => response.text())
+        .then(function (text) {
+            //Parse the CSV file data into a JSON object.
+            //For faster cross referencing, create an object that indexes the state and county ids.
+            var populationInfo = [];
+
+            //Split the lines of the file.
+            var lines = text.split('\n');
+            var row, stateId, countyId, censusPop, obj, j;
+
+            //Skip the header row and then parse each row into an object.
+            for (var i = 1; i < lines.length; i++) {
+                row = lines[i].split(',');
+
+                stateId = row[0];
+                countyId = row[1];
+
+                if (!populationInfo[stateId]) {
+                    populationInfo[stateId] = {};
+                }
+
+                //Get the 2000 population value.
+                censusPop = parseFloat(row[4]);
+
+                //Create an object to parse the CSV row into.
+                obj = {
+                    CountyName: row[3],
+                    StateName: row[2],
+                    CensusPop2000: censusPop
+                };
+
+                //Calculate the population % difference from 2000 census, and round to 1 decimal place.
+                for (j = 5; j < row.length; j++) {
+                    obj['PopChange' + (j - 5)] = Math.round((parseFloat(row[j]) / censusPop - 1) * 100 * 10) / 10;
+                }
+
+                populationInfo[stateId][countyId] = obj;
+            }
+
+            //Download the county boundary GeoJSON data.
+            fetch('data/geojson/US_County_Boundaries.json')
+                .then(function (response) {
+                    return response.json();
+                }).then(function (response) {
+                    var features = response.features;
+
+                    //Loop through each feature and cross reference the population data information.
+                    for (var i = 0; i < features.length; i++) {
+                        var prop = features[i].properties;
+
+                        if (populationInfo[prop['STATE']] && populationInfo[prop['STATE']][prop['COUNTY']]) {
+                            features[i].properties = Object.assign(prop, populationInfo[prop['STATE']][prop['COUNTY']]);
+                        }
+                    }
+
+                    //Add the feature data to the data source.
+                    datasource.add(features);
+
+                    //Create an animation loop. 
+                    timer = new FrameAnimationTimer(function (progress, frameIdx) {
+                        //Update the fill color expression for the current frame.
+                        animatedChoroplethLayer.setOptions({
+                            fillColor: colorExpressions[frameIdx]
+                        });
+
+                        //Update the year in the legend.
+                        document.getElementsByClassName('animatedlegend-label')[0].innerText = (2000 + frameIdx) + '';
+                    }, colorExpressions.length, 10000, true);
+
+                    document.getElementById('playPauseBtn').disabled = '';
+                });
+        });
+}
+
+
+function createLegendScaleBar() {
+    var canvas = document.getElementById('animatedLegendCanvas');
+    var ctx = canvas.getContext('2d');
+
+    //Create a linear gradient for the legend.
+    var grd = ctx.createLinearGradient(0, 0, 256, 0);
+    grd.addColorStop(0, 'rgb(255,0,255)'); // Magenta
+    grd.addColorStop(0.25, 'rgb(0,0,255)'); // Blue
+    grd.addColorStop(0.5, 'rgb(0,255,0)'); // Green
+    grd.addColorStop(0.75, 'rgb(255,255,0)'); // Yellow
+    grd.addColorStop(1, 'rgb(255,0,0)'); // Red
+
+    ctx.fillStyle = grd;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function togglePlayPause() {
+    if (isPaused) {
+        timer.play();
+    } else {
+        timer.pause();
+    }
+
+    isPaused = !isPaused;
+}
+
+function FrameAnimationTimer(renderFrameCallback, numFrames, duration, loop) {
+    var _timerId,
+        frameIdx = 0,
+        _isPaused = false;
+
+    duration = (duration && duration > 0) ? duration : 1000;
+
+    delay = duration / (numFrames - 1);
+
+    this.play = function () {
+        if (renderFrameCallback) {
+            if (_timerId) {
+                _isPaused = false;
+            } else {
+                _timerId = setInterval(function () {
+                    if (!_isPaused) {
+                        var progress = (frameIdx * delay) / duration;
+
+                        renderFrameCallback(progress, frameIdx);
+
+                        if (progress >= 1) {
+                            if (loop) {
+                                frameIdx = 0;
+                            } else {
+                                reset();
+                            }
+                        }
+
+                        frameIdx++;
+                    }
+                }, delay);
+            }
+        }
+    };
+
+    this.pause = function () {
+        _isPaused = true;
+    };
+
+    this.stop = function () {
+        reset();
+    };
+
+    function reset() {
+        if (_timerId != null) {
+            clearInterval(_timerId);
+        }
+
+        frameIdx = 0;
+        _isPaused = false;
+    }
+};
+
+
+
+
 
 
 
@@ -158,7 +385,7 @@ function createLegend(id) {
 // When the entire block is closed. Remove all of those layers and close the child tabs
 $("#choroplethItems").click(function () {
     if ($(this).is(":checked")) {
-    
+
     } else {
         try {
             $("#simpleChoropleth").prop('checked', false);
@@ -212,3 +439,21 @@ $("#extrudedChoropleth").click(function () {
         removeLayer(MyLayers.extrudedChoroplethLayer)
     }
 });
+
+// simpleChoropleth
+$("#animatedChoropleth").click(function () {
+    if ($(this).is(":checked")) {
+        $('.animatedChoroplethInfo').css({
+            display: "block"
+        });
+        addAnimatedChoropleth();
+        createLegendScaleBar();
+    } else {
+        $('.animatedChoroplethInfo').css({
+            display: "none"
+        });
+        $(".animatedLegend").remove();
+        // removeLayer(MyLayers.choroplethLayer)
+    }
+});
+
--- nodeAzureApp/app/routing/htmlRoutes.js
+++ nodeAzureApp/app/routing/htmlRoutes.js
@@ -15,13 +15,6 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/survey.html"));
-  });
-//   app.get('/index.html',function(req,res){
-//     res.sendFile(path.join(__dirname + '/../public/index.html')); 
-//  });
-  // If no matching route is found default to home
   app.get('/', function(req, res) {
     res.redirect('index.html');
 });
