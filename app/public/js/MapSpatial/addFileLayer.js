edbe05c0d2a210ab8ad1685f168c526fc6490ca0
Zach Holland <zack09holland@gmail.com>
2020-03-24 18:46:21 -0700

    got the modal layout configured and files to preview in the preview map

--- app/public/js/MapSpatial/addFileLayer.js
+++ app/public/js/MapSpatial/addFileLayer.js
@@ -1,8 +1,100 @@
 //Create a popup.
 var popup = new atlas.Popup();
+var previewpopup = new atlas.Popup();
 //Add a layers for rendering the data.
-var layers;
+var layers, previewlayers, shpWorker;
+var polygongonPreviewLayer,linePreviewLayer, pointPreviewLayer;
+////////////////////////////////////////////
 
+// loadShapeFile
+
+////////////////////////////////////////////
+function createPreviewMap(){
+     //Initialize a map instance.
+     previewMap = new atlas.Map('previewMap', {
+        // center: [-98.493928, 38.096798],
+        // zoom: 4,
+        // view: "Auto",
+        center: [-94.6, 39.1],
+        zoom: 3,
+        view: 'Auto',
+        style: 'grayscale_light',
+        //Add your Azure Maps subscription key to the map SDK. Get an Azure Maps key at https://azure.com/maps
+        authOptions: {
+            authType: 'subscriptionKey',
+            subscriptionKey: 'TfUWvWqVnTGKGMcIvxr5coNt7eiWrKxh6wJe0keVZSs'
+        }
+    });
+    previewMap.events.add('ready', function () {
+        //Create a data source to add your data to.
+        datasource = new atlas.source.DataSource();
+        previewMap.sources.add(datasource);
+
+        //Create a popup.
+        previewpopup = new atlas.Popup();
+
+        // //Add a layers for rendering the data.
+        // polygongonPreviewLayer = [new atlas.layer.PolygonLayer(datasource, null, {
+        //         filter: ['any', ['==', ['geometry-type'], 'Polygon'],
+        //             ['==', ['geometry-type'], 'MultiPolygon']
+        //         ] //Only render Polygon or MultiPolygon in this layer.
+        //     }),
+        //     new atlas.layer.LineLayer(datasource, null, {
+        //         strokeColor: 'white',
+        //         strokeWidth: 2,
+        //         filter: ['any', ['==', ['geometry-type'], 'Polygon'],
+        //             ['==', ['geometry-type'], 'MultiPolygon']
+        //         ] //Only render Polygon or MultiPolygon in this layer.
+        //     })
+        // ]
+        // linePreviewLayer = [
+        //     new atlas.layer.LineLayer(datasource, null, {
+        //         strokeColor: 'red',
+        //         filter: ['any', ['==', ['geometry-type'], 'LineString'],
+        //             ['==', ['geometry-type'], 'MultiLineString']
+        //         ] //Only render LineString or MultiLineString in this layer.
+        //     })
+        // ]
+        // pointPreviewLayer = [
+        //     new atlas.layer.BubbleLayer(datasource, null, {
+        //         filter: ['any', ['==', ['geometry-type'], 'Point'],
+        //             ['==', ['geometry-type'], 'MultiPoint']
+        //         ] //Only render Point or MultiPoints in this layer.
+        //     })
+        // ];
+        
+        // previewMap.layers.add(polygongonPreviewLayer, 'labels');
+        // previewMap.layers.add(linePreviewLayer, 'labels');
+        // previewMap.layers.add(pointPreviewLayer, 'labels');
+
+        previewlayers = [
+            new atlas.layer.PolygonLayer(datasource, null, {
+                filter: ['any', ['==', ['geometry-type'], 'Polygon'], ['==', ['geometry-type'], 'MultiPolygon']]	//Only render Polygon or MultiPolygon in this layer.
+            }),
+            new atlas.layer.LineLayer(datasource, null, {
+                strokeColor: 'white',
+                strokeWidth: 2,
+                filter: ['any', ['==', ['geometry-type'], 'Polygon'], ['==', ['geometry-type'], 'MultiPolygon']]	//Only render Polygon or MultiPolygon in this layer.
+            }),
+            new atlas.layer.LineLayer(datasource, null, {
+                strokeColor: 'red',
+                filter: ['any', ['==', ['geometry-type'], 'LineString'], ['==', ['geometry-type'], 'MultiLineString']]	//Only render LineString or MultiLineString in this layer.
+            }),
+            new atlas.layer.BubbleLayer(datasource, null, {
+                filter: ['any', ['==', ['geometry-type'], 'Point'], ['==', ['geometry-type'], 'MultiPoint']] //Only render Point or MultiPoints in this layer.
+            })
+        ];
+        previewMap.layers.add(previewlayers, 'labels');
+
+        var wfunc = function (base, cb) {
+            importScripts('data/scripts/shp.min.js');
+            shp(base).then(cb);
+        };
+
+        shpWorker = cw({ data: wfunc }, 2);
+
+    })
+}
 function createLayers(){
     layers = [
         new atlas.layer.PolygonLayer(datasource, null, {
@@ -22,23 +114,41 @@ function createLayers(){
         })
     ];
     map.layers.add(layers, 'labels');
-
     //Add a click event to the layers to show a popup of what the user clicked on.
     map.events.add('click', layers, featureClicked);
-}
 
+}
+////////////////////////////////////////////
+
+// loadShapeFile functions
+
+////////////////////////////////////////////
 function loadShapeFile(url) {
     popup.close();
+    // OLD SHAPEFILE LOADER
+    // CHANGED TO WORK WITH LARGER FILES AS WELL
 
-    shp(url).then(function (data) {
-        //Load the shapefile into the data source and overwrite any existing data. 
-        datasource.setShapes(data);
+    // shp(url).then(function (data) {
+    //     //Load the shapefile into the data source and overwrite any existing data. 
+    //     datasource.setShapes(data);
 
+    //     //Bring the data into view on the map.
+    //     previewMap.setCamera({
+    //         bounds: atlas.data.BoundingBox.fromData(data),
+    //         padding: 50
+    //     });
+    // });
+
+    shpWorker.data(cw.makeUrl(url)).then(function (data) {
+        //Load the shapefile into the data source.
+        datasource.add(data);
         //Bring the data into view on the map.
-        map.setCamera({
+        previewMap.setCamera({
             bounds: atlas.data.BoundingBox.fromData(data),
             padding: 50
         });
+
+        // document.getElementById('loadingIcon').style.display = 'none';
     });
 }
 
@@ -81,7 +191,20 @@ function featureClicked(e) {
         popup.open(map);
     }
 }
+////////////////////////////////////////////
 
+// addFileLayer
+//  -Used to add basic spatial files
+//  -function supports reading:
+//      - KML 
+//      - KMZ 
+//      - GPX
+//      - GeoRSS
+//      - GML
+//      - GeoJSON
+//      - CSV (with spatial columns).
+
+////////////////////////////////////////////
 
 function addFileLayer(fileName) {
     //Create a data source and add it to the map.
@@ -113,8 +236,12 @@ function addFileLayer(fileName) {
     });
 }
 
+/////////////////////////////////////////////////////
 
-// map
+// Functions to add and upload file(s) to the server
+
+/////////////////////////////////////////////////////
+
 var fileNameURL;
 var JSONFile
 function stopDefault(event) {
@@ -151,7 +278,8 @@ function submitFilesForm(form) {
             if (x.status == 200) {
                 JSONFile = JSON.parse(x.responseText);
                 console.log(JSONFile[0])
-                fileNameURL = '/data' + JSONFile[0]
+                fileNameURL = '/data/File Uploads' + JSONFile[0]
+                MyFiles.newFile = fileNameURL
                 fileName = fileNameURL.split('.')[0]
                 fileExtension = fileNameURL.split('.')[1]
                 //Check the file extensions for the file being uploaded
@@ -167,7 +295,7 @@ function submitFilesForm(form) {
     
                     }    
                 } catch (error) {
-                    console.log(fileExtension)
+                    console.log(error)
 
                 }
             
@@ -181,6 +309,19 @@ function submitFilesForm(form) {
     return false;
 }
 
+/////////////////////////////////////////////////////
+
+// Jquwey Onclick events
+
+/////////////////////////////////////////////////////
+
+// Save file
+$("#saveFileBtn").click(function () {
+    console.log(MyFiles.newFile)
+});
+$("#lineStringSelect").select(function () {
+    console.log("its a line")
+});
 
 
 var fileupload = $("#filesfld");
