482feb4628adb9f86b941392ab9dc9f5ec64163a
Zach Holland <zack09holland@gmail.com>
2020-03-29 22:54:12 -0700

    Changed some of the cors policies

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const upload = require("express-fileupload");
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type:application/xml, Authorization, Content-Length, X-Requested-With');
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
